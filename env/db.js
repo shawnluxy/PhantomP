586a8c198d1c0332ca81b68b6bfbe67965ef3fee
shawnluxy <shawnluxy@gmail.com>
2017-07-30 00:14:05 -0400

    check connection state

--- env/db.js
+++ env/db.js
@@ -2,17 +2,19 @@ const mongo = require('mongoose');
 const dbUrl = 'mongodb://localhost:27017/Phantom';
 
 exports.setup = function () {
-    var options = {
-        // user: 'root',
-        // pass: './root'
-    };
-    var db = mongo.createConnection(dbUrl, options);
+    mongo.Promise = global.Promise;
+    var db = connect();
     db.on('error', console.error.bind(console, 'connection error:'));
-    db.on('disconnected', reconnect);
+    db.on('disconnected', connect);
+    // console.log(mongo.connection.readyState);
     return db;
 };
 
-function reconnect () {
-    var option = {server: {socketOptions: {keepAlive: 1}}};
+function connect () {
+    var option = {
+        // user: 'root',
+        // pass: './root'
+        server: {socketOptions: {keepAlive: 1}}
+    };
     return mongo.connect(dbUrl, option).connection;
 }
