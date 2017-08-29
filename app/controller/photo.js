df266b5957fd1cbf709c073b9f84dd0faa65e9b0
shawnluxy <shawnluxy@gmail.com>
2017-08-29 01:13:25 -0400

    add 'delete' API

--- app/controller/photo.js
+++ app/controller/photo.js
@@ -17,9 +17,7 @@ router.get('/find/:id', function (req, res) {
     Photo.findOne({_id: id}, function (err, photo) {
         if(err){
             res.send('FAIL:'+err);
-            return console.error('{photo/find} '+err);
-        }
-        if(photo===null){
+        } else if(photo===null){
             res.send('EMPTY');
         } else{
             res.send(photo);
@@ -32,9 +30,7 @@ router.post('/upload/:uid', function (req, res) {
     User.findOne({_id: uid}, function (err, user) {
         if(err){
             res.send('FAIL:'+err);
-            return console.error('{photo/upload|find} '+err);
-        }
-        if(user===null){
+        } else if(user===null){
             res.send('EMPTY');
         } else{
             var storage = multer.diskStorage({
@@ -47,25 +43,47 @@ router.post('/upload/:uid', function (req, res) {
             var upload = multer({ storage: storage, limits: { fileSize: 1000000 } }).array('photo', 9);
             upload(req, res, function (err) {
                 if(err){
-                    res.send('FAIL:'+err);
-                    return console.error('{photo/upload} '+err);
+                    return res.send('FAIL:'+err);
                 }
                 var files = req.files;
                 for(var i=0; i<files.length; i++){
                     var photo = new Photo;
                     photo.userID = user._id;
                     photo.image = fs.readFileSync(files[i].path);
+                    photo.name = files[i].filename;
                     photo.contentType = files[i].mimetype;
                     photo.save(function (err, p) {
                         if(err){
-                            res.send('FAIL:'+err);
-                            return console.error('{photo/upload|save} '+err);
+                            return res.send('FAIL:'+err);
                         } else {
-                            user.addPhoto(p._id, user._id);
+                            var error = user.addPhoto(p._id, user._id);
+                            if(error) return res.send('FAIL:'+error);
                         }
                     });
                 }
                 res.send('SUCCESS');
+            });
+        }
+    });
+});
+
+router.delete('/delete', function (req, res) {
+    var pid = req.body.pid;
+    Photo.findOneAndRemove({_id: pid}, function (err, photo) {
+        if(err){
+            res.send('FAIL:'+err);
+        } else if(photo===null){
+            res.send('EMPTY');
+        } else{
+            var uid = photo.userID;
+            User.findOne({_id: uid}, function (err, user) {
+                if(err){
+                    return res.send('FAIL:'+err);
+                } else if(user!==null){
+                    var error = user.removePhoto(pid, uid);
+                    if(error) return res.send('FAIL:'+error);
+                }
+                res.send('DELETED: ' + photo._id);
             });
         }
     });
