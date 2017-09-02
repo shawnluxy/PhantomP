a6de3ae03be9ddd93a11781715b00e705f99d4dd
shawnluxy <shawnluxy@gmail.com>
2017-09-02 17:35:46 -0400

    add email-verify, hash password, change respond style

--- app/controller/user.js
+++ app/controller/user.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 const User = require('../model/user').getUserModel();
+const bcrypt = require('bcrypt');
+const nev = require('./emailv');
+
 
 router.use(function (req, res, next) {
     next();
@@ -14,24 +17,23 @@ router.get('/find/:id', function (req, res) {
     var fields = 'username nickname active portrait photo';
     User.findOne({_id: id}, fields, function (err, user) {
         if(err){
-            res.send('FAIL:'+err);
+            res.status(400).json({msg: err});
         } else if(user===null){
-            res.send('EMPTY');
+            res.status(404).json({msg: 'EMPTY'});
         } else{
-            res.send(user);
+            res.status(200).json({msg: 'OK', res: user});
         }
     });
 });
 
 router.post('/create', function (req, res) {
     var user = new User(req.body);
-    user.save(function (err, u) {
-        if(err){
-            res.send('FAIL:'+err);
-        } else {
-            res.send(u._id);
-        }
-    });
+    nev.createUser(user, res);
+});
+
+router.get('/emailVerification/:URL', function (req, res) {
+    var url = req.params.URL;
+    nev.confirmUser(url, res);
 });
 
 router.post('/login', function (req, res) {
@@ -40,16 +42,20 @@ router.post('/login', function (req, res) {
     var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     User.findOne({username: name}, function (err, user) {
         if(err){
-            res.send('FAIL:'+err);
+            res.status(400).json({msg: err});
         } else if(user === null){
-            res.send('USER NOT EXISTS');
-        } else if(!user.checkPwd(pwd)){
-            res.send('PASSWORD INCORRECT');
-        } else if(!user.active){
-            res.send('INACTIVE');
-        } else{
-            user.addIP(ip, user._id);
-            res.send(user._id);
+            res.status(404).json({msg: 'USER NOT EXISTS'});
+        } else {
+            bcrypt.compare(pwd, user.password, function(err, result) {
+                if(!result){
+                    res.status(417).json({msg: 'PASSWORD INCORRECT'});
+                } else {
+                    user.addIP(ip, user._id, function (err) {
+                        if(err) console.log(err);
+                    });
+                    res.status(200).json({msg: 'OK', res: user._id});
+                }
+            });
         }
     });
 });
@@ -57,26 +63,38 @@ router.post('/login', function (req, res) {
 router.put('/update/:id', function (req, res) {
     var id = req.params.id;
     var updates = req.body;
-    User.findOneAndUpdate({_id: id}, updates, {new: true}, function (err, user) {
-        if(err){
-            res.send('FAIL:'+err);
-        } else if(user===null){
-            res.send('EMPTY');
-        } else{
-            res.send(user);
-        }
-    });
+    var userUpdate = function (uid, updates, res) {
+        User.findOneAndUpdate({_id: uid}, updates, {new: true}, function (err, user) {
+            if(err){
+                res.status(400).json({msg: err});
+            } else if(user===null){
+                res.status(404).json({msg: 'EMPTY'});
+            } else{
+                res.status(200).json({msg: 'OK', res: user});
+            }
+        });
+    };
+    if(updates.hasOwnProperty('password')) {
+        bcrypt.genSalt(12, function(err, salt) {
+            bcrypt.hash(updates.password, salt, function(err, hash) {
+                updates.password = hash;
+                userUpdate(id, updates, res);
+            });
+        });
+    } else {
+        userUpdate(id, updates, res);
+    }
 });
 
 router.delete('/delete', function (req, res) {
     var id = req.body.id;
     User.findOneAndRemove({_id: id}, function (err, user) {
         if(err){
-            res.send('FAIL:'+err);
+            res.status(400).json({msg: err});
         } else if(user===null){
-            res.send('EMPTY');
+            res.status(404).json({msg: 'EMPTY'});
         } else{
-            res.send('DELETED: ' + user._id);
+            res.status(200).json({msg: 'DELETED', res: user._id});
         }
     });
 });
